32b6ed34fcc39f8fd71944d774b3012eeafda039
M.B Shuvo <58031584+mbshuvo325@users.noreply.github.com>
2024-10-28 23:44:09 +0600

    update speed

---
+++ android/app/src/main/kotlin/com/chatibot/app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.chatibot.app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- lib/modules/auth/registration/model/registration_response.dart
+++ lib/modules/auth/registration/model/registration_response.dart
@@ -1,6 +1,4 @@
 
-
-
 class SignUpResponse {
   String? message;
   int? nonce;
